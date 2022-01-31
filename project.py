cc0bd2cdc8acd12f43847e0820986f9b26a2c679
iisturiz <73121979+iisturiz@users.noreply.github.com>
2022-01-31 15:30:50 +0100

    Update project.py

--- project.py
+++ project.py
@@ -1,3 +1,7 @@
 """
 This script is for image analysis
 """
+from readlif.reader import LifFile
+import numpy as np
+new = LifFile('/Users/iisturizpetitj/Desktop/20211223_P1_MCF7 #1.lif')
+new.get_image(0)
